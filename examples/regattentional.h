289dc2b7eeb1a9244a986c4fa1ee1424cbaa21d7
kaishengyao <kaisheny@KAISHENGLP1.redmond.corp.microsoft.com>
2015-09-19 08:18:00 -0700

    attention model for regression

--- examples/regattentional.h
+++ examples/regattentional.h
@@ -11,7 +11,7 @@
 #include "cnn/dglstm.h"
 #include "cnn/dict.h"
 #include "cnn/expr.h"
-#include "expr-xtra.h"
+#include "cnn/expr-xtra.h"
 
 #include <algorithm>
 #include <queue>
@@ -39,6 +39,8 @@ struct RegAttentionalModel{
 
     Expression BuildGraph(const std::vector<vector<cnn::real>> &source,
         const std::vector<vector<cnn::real>>& target, ComputationGraph& cg);
+    Expression BuildGraph(FCorpus &source,
+        FCorpus& target, size_t mbsize, ComputationGraph& cg);
 
     std::vector<Parameters*> p_h0, p_f0, p_b0;
     Parameters* p_R;
@@ -58,8 +60,11 @@ struct RegAttentionalModel{
 
     // statefull functions for incrementally creating computation graph, one
     // target word at a time
-    void RegAttentionalModel<Builder>::start_new_instance(const std::vector<vector<cnn::real>> &source, ComputationGraph &cg);
+    void start_new_instance(const std::vector<vector<cnn::real>> &source, ComputationGraph &cg);
+    void start_new_instance(FCorpus& source, FCorpus& target, size_t mbsize, ComputationGraph &cg);
     Expression step(int t, ComputationGraph &cg, RNNPointer *prev_state = 0);
+    Expression step(int t, ComputationGraph &cg, size_t nutt, RNNPointer* prev_state = 0);
+
 
     // state variables used in the above two methods
     Expression src;
@@ -74,6 +79,7 @@ struct RegAttentionalModel{
     std::vector<Expression> i_h0;
     Expression i_Ta;
     std::vector<Expression> src_fwd, src_bwd;
+    std::vector<Expression> vy; /// the target to predict
     unsigned slen;
 };
 
@@ -82,7 +88,7 @@ RegAttentionalModel<Builder>::RegAttentionalModel(cnn::Model& model,
     unsigned _feat_dim, unsigned layers,
     unsigned hidden_dim, unsigned align_dim, bool _rnn_src_embeddings,
     unsigned hidden_replicates)
-    : layers(layers), builder(layers, (_rnn_src_embeddings) ? 2 * hidden_dim : hidden_dim, hidden_dim, &model),
+    : layers(layers), builder(layers, hidden_dim, hidden_dim, &model),
     feat_dim(_feat_dim),
     builder_src_fwd(1, _feat_dim, hidden_dim, &model),
   builder_src_bwd(1, _feat_dim, hidden_dim, &model),
@@ -112,16 +118,10 @@ RegAttentionalModel<Builder>::RegAttentionalModel(cnn::Model& model,
 
     p_Wa = model.add_parameters({ long(align_dim), long(hidden_dim) });
     p_P = model.add_parameters({ long(hidden_dim), long(hidden_dim) });
-    if (rnn_src_embeddings) {
-        p_Ua = model.add_parameters({ long(align_dim), 2 * long(feat_dim) });
-        p_Q = model.add_parameters({ long(2 * hidden_dim), 2*long(feat_dim) });
-        p_Qa = model.add_parameters({ long(2 * hidden_dim) });
-    }
-    else {
-        p_Ua = model.add_parameters({ long(align_dim), long(feat_dim) });
-        p_Q = model.add_parameters({ long(hidden_dim), long(feat_dim) });
-        p_Qa = model.add_parameters({ long(hidden_dim) });
-    }
+    p_Ua = model.add_parameters({ long(align_dim), long(feat_dim) });
+    p_Q = model.add_parameters({ long(hidden_dim), long(feat_dim) });
+    p_Qa = model.add_parameters({ long(hidden_dim) });
+    
     p_va = model.add_parameters({ long(align_dim) });
 }
 
@@ -133,36 +133,20 @@ void RegAttentionalModel<Builder>::start_new_instance(const std::vector<vector<c
     
     slen = source.size();
 
-    if (!rnn_src_embeddings) {
-        src_fwd.resize(slen);
-        for (unsigned i = 0; i < slen; ++i)
-        {
-            long fdim = source[i].size();
-            assert(fdim == feat_dim);
-            src_fwd[i] = input(cg, { fdim }, &source[i]);
-        }
-        src = concatenate_cols(src_fwd);
-    } else {
-	    builder_src_fwd.new_graph(cg);
-        i_h0.clear();
-        for (const auto &p : p_f0)
-            i_h0.push_back(parameter(cg, p));
-        builder_src_fwd.start_new_sequence(i_h0);
-
-        builder_src_bwd.new_graph(cg);
-        i_h0.clear();
-        for (const auto &p : p_b0)
-            i_h0.push_back(parameter(cg, p));
-        builder_src_bwd.start_new_sequence(i_h0);
-        
-        src = bidirectional(slen, source, cg, builder_src_fwd, builder_src_bwd, src_fwd, src_bwd); 
+    src_fwd.resize(slen);
+    for (unsigned i = 0; i < slen; ++i)
+    {
+        long fdim = source[i].size();
+        assert(fdim == feat_dim);
+        src_fwd[i] = input(cg, { fdim }, &source[i]);
     }
-
+    src = concatenate_cols(src_fwd);
+    
     // now for the target sentence
     builder.new_graph(cg); 
     i_h0.clear();
     for (const auto &p: p_h0)
-        i_h0.push_back(parameter(cg, p));
+        i_h0.push_back(concatenate_cols(vector<Expression>(nutt, parameter(cg, p))));
     builder.start_new_sequence(i_h0);
     i_R = parameter(cg, p_R); // hidden -> word rep parameter
     i_P = parameter(cg, p_P); // direct from hidden to output
@@ -173,6 +157,47 @@ void RegAttentionalModel<Builder>::start_new_instance(const std::vector<vector<c
     i_Ua = parameter(cg, p_Ua);
     i_va = parameter(cg, p_va);
     i_uax = i_Ua * src;
+}
+
+template <class Builder>
+void RegAttentionalModel<Builder>::start_new_instance(FCorpus& source, FCorpus& target, size_t mbsize, ComputationGraph &cg)
+{
+    src_fwd.clear();
+    src_bwd.clear();
+
+    slen = mbsize;
+    size_t nutt = source.size();
+
+    src_fwd.resize(slen);
+    FCorpusPointers fp;
+    fp.push_back(&source);
+    fp.push_back(&target);
+
+    vector<vector<Expression>> src_tgt = pack_obs(fp, mbsize, cg); 
+    cg.incremental_forward();
+    src_fwd = src_tgt[0];
+    src = concatenate_cols(src_fwd);
+    cg.incremental_forward();
+    vy = src_tgt[1];
+
+    // now for the target sentence
+    builder.new_graph(cg);
+    i_h0.clear();
+    for (const auto &p : p_h0)
+        i_h0.push_back(concatenate_cols(vector<Expression>(nutt,parameter(cg, p))));
+    builder.start_new_sequence(i_h0);
+    builder.set_data_in_parallel(nutt);
+
+    i_R = parameter(cg, p_R); // hidden -> word rep parameter
+    i_P = parameter(cg, p_P); // direct from hidden to output
+    i_Q = parameter(cg, p_Q); // direct from input to output
+    i_Qa = parameter(cg, p_Qa); // direct from input to output
+    i_bias = parameter(cg, p_bias);  // word bias
+    i_Wa = parameter(cg, p_Wa);
+    i_Ua = parameter(cg, p_Ua);
+    i_va = parameter(cg, p_va);
+    i_uax = i_Ua * src;
+
 }
 
 template <class Builder>
@@ -212,6 +237,61 @@ Expression RegAttentionalModel<Builder>::step(int t, ComputationGraph &cg, RNNPo
 }
 
 template <class Builder>
+Expression RegAttentionalModel<Builder>::step(int t, ComputationGraph &cg, size_t nutt, RNNPointer *prev_state)
+{
+    Expression i_r_t;
+    try{
+
+        vector<Expression> i_o_t;
+        vector<Expression> i_wah_rep; 
+        auto i_h_tm1 = (t == 0 && i_h0.size() > 0) ? i_h0.back() : builder.final_h().back();
+        Expression h_h_tm1 = reshape(i_h_tm1, {long(nutt * HIDDEN_DIM)});
+        for (size_t k = 0; k < nutt; k++)
+        {
+            Expression i_h_tm1 = pickrange(h_h_tm1, k * HIDDEN_DIM , (k + 1)*HIDDEN_DIM);
+
+            Expression i_wah = i_Wa * i_h_tm1;
+
+            /// need to do subsampling
+            i_wah_rep.push_back(concatenate_cols(std::vector<Expression>(slen, i_wah)));
+        }
+
+        Expression i_wah = concatenate_cols(i_wah_rep);
+        Expression i_e_t;
+        i_e_t = transpose(tanh(i_wah + i_uax)) * i_va;
+
+        Expression i_alpha_t; 
+        Expression src_reshaped = reshape(src, { (long) (feat_dim * slen * nutt) });
+        vector<Expression> v_input;
+        for (size_t k = 0; k < nutt; k++)
+        {
+            Expression i_input;
+            Expression i_src = pickrange(src_reshaped, feat_dim * k * slen, feat_dim * (k + 1)*slen);
+            Expression i_src_reshaped = reshape(i_src, { (long)feat_dim, (long)(slen) });
+            i_input = i_src_reshaped * softmax(pickrange(i_e_t, k * slen, (k + 1) * slen));
+
+            Expression input = i_Q * i_input + i_Qa;
+            v_input.push_back(input);
+        }
+
+        Expression i_y_t;
+        if (prev_state)
+            i_y_t = builder.add_input(*prev_state, concatenate_cols(v_input));
+        else
+            i_y_t = builder.add_input(concatenate_cols(v_input));
+
+        Expression i_bias_mb = concatenate_cols(vector<Expression>(nutt, i_bias));
+        i_r_t = affine_transform({ i_bias_mb, i_R, i_y_t });
+    }
+    catch (...)
+    {
+        cerr << "attentional.h :: add_input error " << endl;
+        abort();
+    }
+    return i_r_t;
+}
+
+template <class Builder>
 Expression RegAttentionalModel<Builder>::BuildGraph(const std::vector<vector<cnn::real>> &source,
     const std::vector<vector<cnn::real>>& target, ComputationGraph& cg)
 {
@@ -240,6 +320,31 @@ Expression RegAttentionalModel<Builder>::BuildGraph(const std::vector<vector<cnn
     }
 
     Expression i_nerr = sum(errs) / nsamples;
+
+    return i_nerr;
+}
+
+template <class Builder>
+Expression RegAttentionalModel<Builder>::BuildGraph( FCorpus &source,
+    FCorpus& target, size_t mbsize, ComputationGraph& cg)
+{
+    //std::cout << "source sentence length: " << source.size() << " target: " << target.size() << std::endl;
+    start_new_instance(source, target, mbsize, cg);
+    cg.incremental_forward();
+
+    size_t nutt = source.size();
+    std::vector<Expression> errs;
+
+    for (unsigned t = 0; t < mbsize; ++t) {
+        Expression i_y_t = step(t, cg, nutt);
+        Expression i_m_t = logistic(i_y_t);
+
+        Expression i_r_t = cwise_multiply(src_fwd[t], i_m_t); /// masked noisy spectra
+        Expression i_ydist = squared_distance(i_r_t, vy[t]);
+        errs.push_back(i_ydist);
+    }
+
+    Expression i_nerr = sum(errs);
 
     return i_nerr;
 }
