5fc59b39c5551514cb6d947e6de4d2af4620f7b2
kaishengyao <kaisheny@KAISHENGLP1.redmond.corp.microsoft.com>
2015-09-25 17:15:37 -0700

    add unidirectional function

--- cnn/expr-xtra.h
+++ cnn/expr-xtra.h
@@ -113,6 +113,74 @@ Expression bidirectional(unsigned & slen, const vector<vector<int>>& source, Com
     return src;
 }
 
+template<class Builder>
+Expression forward_directional(unsigned & slen, const vector<vector<int>>& source, ComputationGraph& cg, LookupParameters* p_cs, vector<cnn::real>& zero,
+    Builder & encoder_fwd, size_t feat_dim)
+{
+    size_t nutt = source.size();
+    /// get the maximum length of utternace from all speakers
+    slen = 0;
+    for (auto p : source)
+        slen = (slen < p.size()) ? p.size() : slen;
+
+    std::vector<Expression> source_embeddings;
+
+    std::vector<Expression> src_fwd(slen);
+
+    Expression i_x_t;
+
+    for (int t = 0; t < slen; ++t) {
+        vector<Expression> vm;
+        for (size_t k = 0; k < nutt; k++)
+        {
+            if (source[k].size() > t)
+                vm.push_back(lookup(cg, p_cs, source[k][t]));
+            else
+                vm.push_back(input(cg, { (long)feat_dim }, &zero));
+        }
+        i_x_t = concatenate_cols(vm);
+        src_fwd[t] = encoder_fwd.add_input(i_x_t);
+    }
+
+    Expression src = concatenate_cols(src_fwd);
+
+    return src;
+}
+
+template<class Builder>
+Expression backward_directional(unsigned & slen, const vector<vector<int>>& source, ComputationGraph& cg, LookupParameters* p_cs, vector<cnn::real>& zero,
+    Builder& encoder_bwd, size_t feat_dim)
+{
+    size_t nutt = source.size();
+    /// get the maximum length of utternace from all speakers
+    slen = 0;
+    for (auto p : source)
+        slen = (slen < p.size()) ? p.size() : slen;
+
+    std::vector<Expression> source_embeddings;
+
+    std::vector<Expression> src_bwd(slen);
+
+    Expression i_x_t;
+
+    for (int t = slen - 1; t >= 0; --t) {
+        vector<Expression> vm;
+        for (size_t k = 0; k < nutt; k++)
+        {
+            if (source[k].size() > t)
+                vm.push_back(lookup(cg, p_cs, source[k][t]));
+            else
+                vm.push_back(input(cg, { (long)feat_dim }, &zero));
+        }
+        i_x_t = concatenate_cols(vm);
+        src_bwd[t] = encoder_bwd.add_input(i_x_t);
+    }
+
+    Expression src = concatenate_cols(src_bwd);
+
+    return src;
+}
+
 /// do forward and backward embedding on continuous valued vectors
 template<class Builder>
 Expression bidirectional(int slen, const vector<vector<cnn::real>>& source, ComputationGraph& cg, Builder & encoder_fwd, Builder& encoder_bwd,
