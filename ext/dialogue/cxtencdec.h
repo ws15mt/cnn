73717cec01f8913a420075302aa2d7e471d9484a
kaishengyao <kaishengy@gmail.com>
2015-12-23 15:30:03 -0800

    add sequence to sequence method for dialogue

--- ext/dialogue/cxtencdec.h
+++ ext/dialogue/cxtencdec.h
@@ -36,30 +36,6 @@ public:
     CxtEncDecModel(cnn::Model& model, int vocab_size_src, int vocab_size_tgt, const vector<size_t>& layers, const vector<unsigned>& hidden_dims, int hidden_replicates, int decoder_use_additional_input = 0, int mem_slots = 0, cnn::real iscale = 1.0) :
         DialogueBuilder(model, vocab_size_src, vocab_size_tgt, layers, hidden_dims, hidden_replicates, decoder_use_additional_input, mem_slots, iscale)
     {
-        hidden_dim = hidden_dims;
-
-        if (hidden_dim[DECODER_LAYER] != hidden_dim[ENCODER_LAYER])
-        {
-            cerr << "wrong dimension of encoder and decoder layer. they should be the same, as they use the same lookup table" << endl;
-            throw("wrong dimension of encoder and decoder layer. they should be the same, as they use the same lookup table");
-        }
-
-        p_cs = model.add_lookup_parameters(long(vocab_size_src), { long(hidden_dim[ENCODER_LAYER]) }, iscale);
-        p_R = model.add_parameters({ long(vocab_size_tgt), long(hidden_dim[DECODER_LAYER]) }, iscale);
-        p_bias = model.add_parameters({ long(vocab_size_tgt) }, iscale);
-
-        p_U = model.add_parameters({ long(hidden_dim[ALIGN_LAYER]), long(2 * hidden_dim[ENCODER_LAYER]) }, iscale);
-
-        for (size_t i = 0; i < layers[ENCODER_LAYER] * rep_hidden; i++)
-        {
-            p_h0.push_back(model.add_parameters({ long(hidden_dim[ENCODER_LAYER]) }, iscale));
-            p_h0.back()->reset_to_zero();
-        }
-        zero.resize(hidden_dim[ENCODER_LAYER], 0);  /// for the no obs observation
-
-        p_cxt2dec_w = model.add_parameters({ long(hidden_dim[DECODER_LAYER]), long(hidden_dim[INTENTION_LAYER]) }, iscale);
-
-        i_h0.clear();
     }
 
 public:
@@ -134,6 +110,129 @@ public:
 
         v_decoder_context.clear();
         v_decoder_context.resize(nutt);
+        for (int t = 0; t < oslen; ++t) {
+            vector<int> vobs;
+            for (auto p : osent)
+            {
+                if (t < p.size())
+                    vobs.push_back(p[t]);
+                else
+                    vobs.push_back(-1);
+            }
+            Expression i_y_t = decoder_step(vobs, cg);
+            Expression i_r_t = i_R * i_y_t;
+
+            Expression x_r_t = reshape(i_r_t, { (long)vocab_size * (long)nutt });
+            for (size_t i = 0; i < nutt; i++)
+            {
+                if (t < osent[i].size() - 1)
+                {
+                    /// only compute errors on with output labels
+                    Expression r_r_t = pickrange(x_r_t, i * vocab_size, (i + 1)*vocab_size);
+                    Expression i_ydist = log_softmax(r_r_t);
+                    errs.push_back(pick(i_ydist, osent[i][t + 1]));
+                    tgt_words++;
+                }
+            }
+        }
+
+        Expression i_nerr = sum(errs);
+
+        turnid++;
+        return -i_nerr;
+    };
+
+    Expression decoder_step(vector<int> trg_tok, ComputationGraph& cg)
+    {
+        size_t nutt = trg_tok.size();
+
+        Expression i_x_t;
+        vector<Expression> v_x_t;
+        for (auto p : trg_tok)
+        {
+            Expression i_x_x;
+            if (p >= 0)
+                i_x_x = lookup(cg, p_cs, p);
+            else
+                i_x_x = input(cg, { (long)hidden_dim[DECODER_LAYER] }, &zero);
+            v_x_t.push_back(i_x_x);
+        }
+        i_x_t = concatenate_cols(v_x_t);
+
+        Expression i_y_t = decoder.add_input(i_x_t);
+
+        return i_y_t;
+    };
+};
+
+/** sequence to sequence encoder decodr 
+*/
+template<class Builder, class Decoder>
+class Seq2SeqEncDecModel : public DialogueBuilder<Builder, Decoder>{
+private:
+    Expression i_R;
+
+public:
+    Seq2SeqEncDecModel(cnn::Model& model, int vocab_size_src, int vocab_size_tgt, const vector<size_t>& layers, const vector<unsigned>& hidden_dims, int hidden_replicates, int decoder_use_additional_input = 0, int mem_slots = 0, cnn::real iscale = 1.0) :
+        DialogueBuilder(model, vocab_size_src, vocab_size_tgt, layers, hidden_dims, hidden_replicates, decoder_use_additional_input, mem_slots, iscale)
+    {
+    }
+
+public:
+
+    void start_new_instance(const std::vector<std::vector<int>> &source, ComputationGraph &cg) override
+    {
+        nutt = source.size();
+
+        if (i_h0.size() == 0)
+        {
+            i_h0.clear();
+            for (auto p : p_h0)
+            {
+                i_h0.push_back(concatenate_cols(vector<Expression>(nutt, parameter(cg, p))));
+            }
+
+            i_R = parameter(cg, p_R); // hidden -> word rep parameter
+        }
+
+        encoder_bwd.new_graph(cg);
+        encoder_bwd.set_data_in_parallel(nutt);
+        encoder_bwd.start_new_sequence();
+
+        /// the source sentence has to be approximately the same length
+        src_len = each_sentence_length(source);
+        for (auto p : src_len)
+        {
+            src_words += (p - 1);
+        }
+
+        /// get the backward direction encoding of the source
+        src_fwd = concatenate_cols(backward_directional<Builder>(slen, source, cg, p_cs, zero, encoder_bwd, hidden_dim[ENCODER_LAYER]));
+
+        v_src = shuffle_data(src_fwd, (size_t)nutt, (size_t)hidden_dim[ENCODER_LAYER], src_len);
+
+        /// for contet
+        /// have input to context RNN
+        vector<Expression> to = encoder_bwd.final_s();
+
+        decoder.new_graph(cg);
+        decoder.set_data_in_parallel(nutt);
+        decoder.start_new_sequence(to);  /// get the intention
+    };
+
+    Expression build_graph(const std::vector<std::vector<int>> &source, const std::vector<std::vector<int>>& osent, ComputationGraph &cg){
+        size_t nutt;
+        start_new_instance(source, cg);
+
+        // decoder
+        vector<Expression> errs;
+
+        nutt = osent.size();
+
+        int oslen = 0;
+        for (auto p : osent)
+            oslen = (oslen < p.size()) ? p.size() : oslen;
+
         for (int t = 0; t < oslen; ++t) {
             vector<int> vobs;
             for (auto p : osent)
