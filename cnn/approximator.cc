779395b951b72ee02bde75f05d93a67a5056ed73
U-REDMOND\kaisheny <kaisheny@kaishengw541.redmond.corp.microsoft.com>
2016-01-18 14:28:29 -0800

    class-based cross-entropy training

--- cnn/approximator.cc
+++ cnn/approximator.cc
@@ -1,4 +1,4 @@
-#include "cnn/dnn.h"
+#include "cnn/approximator.h"
 
 #include <string>
 #include <cassert>
@@ -7,6 +7,7 @@
 #include <boost/lexical_cast.hpp>
 #include "cnn/nodes.h"
 #include "cnn/expr-xtra.h"
+#include <fstream>
 
 using namespace std;
 using namespace cnn::expr;
@@ -14,33 +15,36 @@ using namespace cnn;
 
 namespace cnn {
 
-    enum { X2C = 0, X2CB, X2CI, X2CIB };
+    enum { X2C = 0, X2CB};
 
     void ClsBasedBuilder::display(ComputationGraph& cg) {
-        for (unsigned i = 0; i < layers; ++i) {
+        display_value(i_cls, cg);
+        display_value(i_cls_bias, cg);
+        for (unsigned i = 0; i < ncls; ++i) {
             const std::vector<Expression>& vars = param_vars[i];
             for (size_t i = 0; i < vars.size(); i++)
                 display_value(vars[i], cg);
         }
     }
 
-    ClsBasedBuilder::ClsBasedBuilder(const vector<int>& cls2nbrwords, /// #words for each class, class starts from 0
-        const vector<long> & acc_cls2size, /// the accumulated class size
+    ClsBasedBuilder::ClsBasedBuilder(
+        const unsigned int input_dim,
+        const vector<int>& cls2nbrwords, /// #words for each class, class starts from 0
         const vector<long>& word2cls,
         const vector<long>& dict_wrd_id2within_class_id,
         Model& model,
         cnn::real iscale,
-        string name) : 
-        clssize(cls2nbrwords), word2cls(word2cls), acc_cls2size(acc_cls2size), dict_wrd_id2within_class_id(dict_wrd_id2within_class_id)
+        string name) : input_dim(input_dim),
+        clssize(cls2nbrwords), word2cls(word2cls), dict_wrd_id2within_class_id(dict_wrd_id2within_class_id)
     {
         unsigned int n_cls = clssize.size();
-        p_cls = model.add_parameters({ n_cls, HIDDEN_DIM }, iscale, name + " to cls");
+        p_cls = model.add_parameters({ n_cls, input_dim }, iscale, name + " to cls");
         p_cls_bias = model.add_parameters({ n_cls }, iscale, name + " to cls bias");
         for (size_t id = 0; id < n_cls; id++)
         {
             unsigned int  clssize = cls2nbrwords[id];
-            p_R.push_back(model.add_parameters({ clssize, HIDDEN_DIM }, iscale, name + " to cls " + id));
-            p_bias.push_back(model.add_parameters({ clssize }, iscale, name + " to cls bias " + id));
+            p_R.push_back(model.add_parameters({ clssize, input_dim }, iscale, name + " to cls " + boost::lexical_cast<string>(id)));
+            p_bias.push_back(model.add_parameters({ clssize }, iscale, name + " to cls bias " + boost::lexical_cast<string>(id)));
         }
     }
 
@@ -57,61 +61,130 @@ namespace cnn {
 
             param_vars.push_back(vars);
         }
+        errors.clear();
         set_data_in_parallel(1);
     }
 
-    void DNNBuilder::set_data_in_parallel(int n)
+    void ClsBasedBuilder::set_data_in_parallel(int n)
     {
         dparallel = n;
 
-        biases.clear();
-        for (unsigned i = 0; i < layers; ++i) {
-            const vector<Expression>& vars = param_vars[i];
-            Expression bimb = concatenate_cols(vector<Expression>(data_in_parallel(), vars[X2HB]));
-
-            vector<Expression> b = { bimb };
-            biases.push_back(b);
-        }
+        errors.clear();
+        errors.resize(n);
     }
 
-    Expression DNNBuilder::add_input_impl(const Expression &in) {
-        h.resize(layers);
+    Expression ClsBasedBuilder::add_input_impl(const Expression &in, long target_wordid, unsigned uttid) {
+        int cls_id = word2cls[target_wordid];
+        Expression newin = reshape(in, { input_dim * dparallel });
+        Expression x = pickrange(newin, uttid * input_dim, (uttid + 1)*input_dim);
+        vector<Expression> param = param_vars[cls_id];
+        Expression i_r_t = param[X2CB] + param[X2C] * x;
+        Expression i_c_t = i_cls_bias + i_cls * x;
+        Expression i_err_cls = pick(log_softmax(i_c_t), cls_id);
+        Expression i_err_prb = pick(log_softmax(i_r_t), dict_wrd_id2within_class_id[target_wordid]);
+        errors[uttid].push_back(-(i_err_cls + i_err_prb));
 
-        Expression x = in;
+        return errors[uttid].back();
+    }
 
-        for (unsigned i = 0; i < layers; ++i) {
-            const vector<Expression>& vars = param_vars[i];
-
-            Expression y = affine_transform({ biases[i][0], vars[0], x });
-
-            x = h[i] = tanh(y);
+    vector<cnn::real> ClsBasedBuilder::respond(const Expression &in, ComputationGraph& cg) 
+    {
+        Expression cls_log_prob = log_softmax(i_cls * in + i_cls_bias);
+        vector<Expression> within_cls_log_prob;
+        for (int cli = 0; cli < clssize.size(); cli++)
+        {
+            vector<Expression> param = param_vars[cli];
+            Expression i_r_t = param[X2CB] + param[X2C] * in;
+            within_cls_log_prob.push_back(log_softmax(i_r_t));
         }
-        return h.back();
+        auto cls_dist = get_value(cls_log_prob, cg);
+        vector<cnn::real> dist;
+        for (int cli = 0; cli < clssize.size(); cli++){
+            auto within_cls_dist = get_value(within_cls_log_prob[cli], cg);
+            vector<cnn::real> vtmp(clssize[cli], cls_dist[cli]);
+            std::transform(within_cls_dist.begin(), within_cls_dist.end(), vtmp.begin(), within_cls_dist.begin(), std::plus<cnn::real>());
+            dist.insert(dist.end(), within_cls_dist.begin(), within_cls_dist.end());
+        }
+
+        return dist;
+    }
+
+    void ClsBasedBuilder::copy(const ClsBasedBuilder& othercls) {
+        const ClsBasedBuilder& clsref = (const ClsBasedBuilder&)othercls;
+        assert(p_R.size() == clsref.p_R.size());
+        assert(p_bias.size() == clsref.p_bias.size());
+        for (size_t i = 0; i < clsref.p_R.size(); ++i) {
+            p_R[i]->copy(*clsref.p_R[i]);
+            p_bias[i]->copy(*clsref.p_bias[i]);
+        }
+        p_cls->copy(*clsref.p_cls);
+        p_cls_bias->copy(*clsref.p_cls_bias);
     }
 
 
-    void DNNBuilder::copy(const DNNBuilder & rnn) {
-        const DNNBuilder& rnn_simple = (const DNNBuilder&)rnn;
-        assert(params.size() == rnn_simple.params.size());
-        for (size_t i = 0; i < rnn_simple.params.size(); ++i) {
-            params[i][0]->copy(*rnn_simple.params[i][0]);
-            params[i][1]->copy(*rnn_simple.params[i][1]);
+    /**
+    dict_wrd_id2within_class_id : the dictionary word id to the id inside a class
+    */
+    void ClsBasedBuilder::load_word2cls_fn(string word2clsfn, Dict& sd, std::vector<long>& wrd2cls, std::vector<long>& dict_wrd_id2within_class_id, std::vector<int> & cls2size)
+    {
+        ifstream in(word2clsfn);
+        string line;
+        /// go through one line at a time to match dictionay
+        /// it is possible that the word clustering may have different words from the dictionary, 
+        /// so go through one more to add missing words into the dictionary
+        vector<string> missing_words;
+        while (getline(in, line)) {
+            std::istringstream in(line);
+            std::string word;
+
+            in >> word;
+            if (sd.Contains(word) == false)
+                missing_words.push_back(word);
+            sd.Convert(word, true);
         }
-    }
+        in.close();
 
-    Expression ReluDNNBuilder::add_input_impl(const Expression &in)  {
-        h.resize(layers);
+        in.open(word2clsfn);
+        wrd2cls.resize(sd.size());
+        dict_wrd_id2within_class_id.resize(sd.size(), -1);
+        map<int, int> cls2acccnt; /// the count for each class so far
+        while (getline(in, line)) {
 
-        Expression x = in;
+            std::istringstream in(line);
+            std::string word;
+            string cls;
 
-        for (unsigned i = 0; i < layers; ++i) {
-            const vector<Expression>& vars = param_vars[i];
+            in >> word;
+            in >> cls;
 
-            Expression y = affine_transform({ biases[i][0], vars[0], x });
+            int icls = boost::lexical_cast<int>(cls)-1;
+            int wridx = -1;
+            if (sd.Contains(word))
+            {
+                wridx = sd.Convert(word, true);
 
-            x = h[i] = rectify(y);
+                wrd2cls[wridx] = icls;
+                if (cls2acccnt.find(icls) == cls2acccnt.end())
+                    cls2acccnt[icls] = 1;
+                else
+                    cls2acccnt[icls] += 1;
+                dict_wrd_id2within_class_id[wridx] = cls2acccnt[icls] - 1;
+            }
         }
-        return h.back();
+        in.close();
+
+        for (auto&p : sd.GetWordList())
+        {
+            long wd = sd.Convert(p);
+            if (wrd2cls[wd] == -1)
+                throw("check word clustering procedure as there are words in dictionary that are not clustered"); 
+        }
+
+        cls2size.clear();
+        for (int i = 0; i < cls2acccnt.size(); i++)
+        {
+            cls2size.push_back(cls2acccnt[i]);
+        }
     }
 
 
--- cnn/approximator.h
+++ cnn/approximator.h
@@ -1,14 +1,17 @@
-#ifndef _DNN_H
-#define _DNN_H
+#ifndef _APPROXIMATOR_H
+#define _APPROXIMATOR_H
 
 /**
-this uses RNN interface but actually is deep network
+this is for approximating criterion.
+1) class-based objective
+To-do: NCE
 */
 #include <string>
 #include <vector>
 #include "cnn/model.h"
 #include "cnn/cnn.h"
 #include "cnn/expr.h"
+#include "cnn/dict.h"
 
 using namespace std;
 using namespace cnn::expr;
@@ -18,20 +21,24 @@ namespace cnn {
     class ClsBasedBuilder{
     public:
         ClsBasedBuilder() { dparallel = 1; }
-        explicit ClsBasedBuilder(unsigned layers,
-            unsigned input_dim,
-            unsigned hidden_dim,
-            unsigned output_dim,
-            Model* model,
-            cnn::real i_scale = 1.0, 
+        explicit ClsBasedBuilder(
+            const unsigned int input_dim,
+            const vector<int>& cls2nbrwords, /// #words for each class, class starts from 0
+            const vector<long>& word2cls,
+            const vector<long>& dict_wrd_id2within_class_id,
+            Model& model,
+            cnn::real iscale,
             string name = "");
-        /// for parameter sharing 
-        ClsBasedBuilder(const DNNBuilder& ref)
+        ClsBasedBuilder(const ClsBasedBuilder& ref)
         {
-            input_dims = ref.input_dims;
-            params = ref.params;
-            param_vars = ref.param_vars;
-            layers = input_dims.size();
+            p_R = ref.p_R;
+            p_bias = ref.p_bias;
+            p_cls = ref.p_cls;
+            p_cls_bias = ref.p_cls_bias;
+            ncls = ref.ncls;
+            clssize = ref.clssize;
+            word2cls = ref.word2cls;
+            dict_wrd_id2within_class_id = ref.dict_wrd_id2within_class_id;
             dparallel = ref.dparallel;
         }
 
@@ -39,15 +46,11 @@ namespace cnn {
 
     protected:
         void new_graph_impl(ComputationGraph& cg);
-        virtual Expression add_input_impl(const Expression& x);
+        virtual Expression add_input_impl(const Expression& x, long target_wordid, unsigned uttid);
 
     public:
-        Expression back() const { return h.back(); }
-        std::vector<Expression> final_h() const { return h; }
-        std::vector<Expression> final_s() const { return final_h(); }
-        void copy(const DNNBuilder & params);
-
-        unsigned num_h0_components() const { return layers; }
+        vector<Expression> back() const { return errors.back(); }
+        void copy(const ClsBasedBuilder& params);
 
         void set_data_in_parallel(int n);
         int data_in_parallel() const { return dparallel; }
@@ -57,9 +60,18 @@ namespace cnn {
 
     public:
         // add another timestep by reading in the variable x
-        // return the hidden representation of the deepest layer
-        Expression add_input(const Expression& x) {
-            return add_input_impl(x);
+        // targetid is a vector of training target. for target dont want to have error signals, set the corresponding element to a negative value
+        // return the errors
+        vector<Expression> add_input(const Expression& x, vector<long> targetid) {
+            vector<Expression> err; 
+            unsigned id = 0;
+            for (auto& p : targetid)
+            {
+                if (p >= 0)
+                    err.push_back(add_input_impl(x, p, id));
+                id++;
+            }
+            return err;
         }
 
         // call this to reset the builder when you are working with a newly
@@ -72,10 +84,13 @@ namespace cnn {
         // call this before add_input and after new_graph,
         // when starting a new sequence on the same hypergraph.
         // h_0 is used to initialize hidden layers at timestep 0 to given values
-        void start_new_sequence(const std::vector<Expression>& h_0 = {}) {
+        void start_new_sequence() {
         }
 
+        vector<cnn::real> respond(const Expression &in, ComputationGraph& cg);
 
+    public:
+        void load_word2cls_fn(string word2clsfn, Dict& sd, std::vector<long>& wrd2cls, std::vector<long>& dict_wrd_id2within_class_id, std::vector<int> & cls2size);
     protected:
 
         vector<Parameters*> p_R;
@@ -83,34 +98,18 @@ namespace cnn {
         Parameters* p_cls, *p_cls_bias;
         Expression i_cls, i_cls_bias;
         unsigned int ncls;
+        unsigned input_dim;
         vector<int> clssize;
         vector<long> word2cls;
-        vector<long> acc_cls2size;
         vector<long> dict_wrd_id2within_class_id;
         
         int dparallel;
 
         std::vector<std::vector<Expression>> param_vars;
+
+        vector<vector<Expression>> errors; /// [nutt][vector<error>] 
     };
 
-    class ReluDNNBuilder  : public DNNBuilder{
-    public:
-        ReluDNNBuilder() { dparallel = 1; }
-        explicit ReluDNNBuilder(unsigned layers,
-            unsigned input_dim,
-            unsigned hidden_dim,
-            unsigned output_dim,
-            Model* model,
-            cnn::real i_scale = 1.0,
-            string name = "") :
-            DNNBuilder(layers, input_dim, hidden_dim, output_dim, model, i_scale)
-        {
-        }
-
-    protected:
-        Expression add_input_impl(const Expression& x) override;
-
-    };
 };
 
 #endif
