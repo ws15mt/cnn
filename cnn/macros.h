94199dbe90d866f5b83ba1b1c5c7d9d13cc04b33
U-REDMOND\kaisheny <kaisheny@kaishengw541.redmond.corp.microsoft.com>
2016-02-03 23:43:00 -0800

    added a macro file

---
+++ cnn/macros.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace cnn {
+
+#define ENCODER_LAYER 0
+#define INTENTION_LAYER 1
+#define DECODER_LAYER 2  
+#define ALIGN_LAYER 3
+
+#define INPUT_LAYER ENCODER_LAYER
+#define HIDDEN_LAYER INTENTION_LAYER
+#define OUTPUT_LAYER DECODER_LAYER
+};
