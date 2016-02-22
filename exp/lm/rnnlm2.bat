71be24de9801f2100bfd4bd18b0884c7c741cf80
U-REDMOND\kaisheny <kaisheny@kaishengw541.redmond.corp.microsoft.com>
2016-02-21 21:57:11 -0800

    update rnnlm2.bat

--- exp/lm/rnnlm2.bat
+++ exp/lm/rnnlm2.bat
@@ -7,7 +7,7 @@ REM
 set WORKDIR=.
 set DATADIR=c:\data\ptbdata
 set BINDIR=C:\dev\cnn\msbuildcuda\examples\Release
-set BINDIR=C:\dev\cnn\msbuild\examples\Release
+set BINDIR=C:\dev\cnn2\msbuildcuda\examples\Release
 
 mkdir %WORKDIR%\logs
 mkdir %WORKDIR%\models
