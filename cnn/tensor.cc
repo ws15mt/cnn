7e6a299da2f3203672340ec318297c5ece1ca4bd
U-REDMOND\kaisheny <kaisheny@kaishengw541.redmond.corp.microsoft.com>
2016-03-05 22:21:38 -0800

    added pushelementstomemory function

--- cnn/tensor.cc
+++ cnn/tensor.cc
@@ -14,7 +14,7 @@ namespace cnn {
 
     ostream& operator<<(ostream& os, const Tensor& t) {
     #if HAVE_CUDA
-      vector<real> vt = as_vector(t);
+      vector<cnn::real> vt = as_vector(t);
       Eigen::Map<EMatrix> m(&vt[0], t.d.rows(), t.d.cols());
       os << m;
     #else
@@ -23,7 +23,7 @@ namespace cnn {
       return os;
     }
 
-    real as_scalar(const Tensor& t) {
+    cnn::real as_scalar(const Tensor& t) {
       assert(t.d.size() == 1);
     #if HAVE_CUDA
       cnn::real res;
@@ -34,20 +34,30 @@ namespace cnn {
     #endif
     }
 
-    vector<real> as_vector(const Tensor& v) {
-      vector<real> res(v.d.size());
+    vector<cnn::real> as_vector(const Tensor& v) {
+      vector<cnn::real> res(v.d.size());
     #if HAVE_CUDA
-      CUDA_CHECK(cudaMemcpy(&res[0], v.v, sizeof(real) * res.size(), cudaMemcpyDeviceToHost));
+      CUDA_CHECK(cudaMemcpy(&res[0], v.v, sizeof(cnn::real) * res.size(), cudaMemcpyDeviceToHost));
     #else
-      memcpy(&res[0], v.v, sizeof(real) * res.size());
+      memcpy(&res[0], v.v, sizeof(cnn::real) * res.size());
     #endif
       return res;
+    }
+
+    vector<cnn::real> as_vector(int nsize, const cnn::real* v) {
+        vector<cnn::real> res(nsize);
+#if HAVE_CUDA
+        CUDA_CHECK(cudaMemcpy(&res[0], v, sizeof(cnn::real) * res.size(), cudaMemcpyDeviceToHost));
+#else
+        memcpy(&res[0], v, sizeof(cnn::real) * res.size());
+#endif
+        return res;
     }
 
     cnn::real TensorTools::AccessElement(const Tensor& v, int index) {
     #if HAVE_CUDA
       cnn::real ret;
-      cudaMemcpy(&ret, &v.v[index], sizeof(real), cudaMemcpyDeviceToHost);
+      cudaMemcpy(&ret, &v.v[index], sizeof(cnn::real), cudaMemcpyDeviceToHost);
       return ret;
     #else
       return v.v[index];
@@ -64,7 +74,7 @@ namespace cnn {
 
     void TensorTools::SetElement(const Tensor& v, int index, cnn::real value) {
     #if HAVE_CUDA
-      cudaMemcpyAsync(&v.v[index], &value, sizeof(real), cudaMemcpyHostToDevice);
+      cudaMemcpyAsync(&v.v[index], &value, sizeof(cnn::real), cudaMemcpyHostToDevice);
     #else
       v.v[index] = value;
     #endif
@@ -72,18 +82,30 @@ namespace cnn {
 
     void TensorTools::SetElements(const Tensor& v, const vector<cnn::real>& vec) {
     #if HAVE_CUDA
-      cudaMemcpyAsync(v.v, &vec[0], sizeof(real) * vec.size(), cudaMemcpyHostToDevice);
+      cudaMemcpyAsync(v.v, &vec[0], sizeof(cnn::real) * vec.size(), cudaMemcpyHostToDevice);
     #else
-      memcpy(v.v, &vec[0], sizeof(real) * vec.size());
+      memcpy(v.v, &vec[0], sizeof(cnn::real) * vec.size());
     #endif
     }
 
     void TensorTools::CopyElements(const Tensor& v, const Tensor& v_src) {
     #if HAVE_CUDA
-      cudaMemcpyAsync(v.v, v_src.v, sizeof(real) * v.d.size(), cudaMemcpyDeviceToDevice);
+      cudaMemcpyAsync(v.v, v_src.v, sizeof(cnn::real) * v.d.size(), cudaMemcpyDeviceToDevice);
     #else
-      memcpy(v.v, v_src.v, sizeof(real) * v.d.size());
+      memcpy(v.v, v_src.v, sizeof(cnn::real) * v.d.size());
     #endif
+    }
+
+    void TensorTools::PushElementsToMemory(int& size, const int buf_size, cnn::real* v, const Tensor& v_src) {
+        if (size + v_src.d.size() > buf_size)
+            runtime_error("PushElementsToMemory : memory exhausted");
+#if HAVE_CUDA
+        /// save to the current available location
+        cudaMemcpyAsync(v+size, v_src.v, sizeof(cnn::real) * v_src.d.size(), cudaMemcpyDeviceToDevice);
+#else
+        memcpy(v+size, v_src.v, sizeof(cnn::real) * v_src.d.size());
+#endif
+        size += v_src.d.size();
     }
 
     void TensorTools::Constant(Tensor& d, cnn::real c) {
@@ -106,13 +128,13 @@ namespace cnn {
       Constant(d, 0);
     }
 
-    void TensorTools::Randomize(Tensor& val, real scale) {
-      uniform_real_distribution<real> distribution(-scale,scale);
+    void TensorTools::Randomize(Tensor& val, cnn::real scale) {
+      uniform_real_distribution<cnn::real> distribution(-scale,scale);
       auto b = [&] {return distribution(*rndeng);};
     #if HAVE_CUDA
       cnn::real* t = new cnn::real[val.d.size()];
       generate(t, t + val.d.size(), b);
-      CUDA_CHECK(cudaMemcpy(val.v, t, sizeof(real) * val.d.size(), cudaMemcpyHostToDevice));
+      CUDA_CHECK(cudaMemcpy(val.v, t, sizeof(cnn::real) * val.d.size(), cudaMemcpyHostToDevice));
       delete[] t;
     #else
       generate(val.v, val.v + val.d.size(), b);
@@ -123,14 +145,14 @@ namespace cnn {
       Randomize(d, sqrt(6) / sqrt(d.d.sum_dims()));
     }
 
-    void TensorTools::RandomBernoulli(Tensor& val, real p, real scale) {
+    void TensorTools::RandomBernoulli(Tensor& val, cnn::real p, cnn::real scale) {
       bernoulli_distribution distribution(p);
       auto b = [&] {return distribution(*rndeng) * scale;};
       generate(val.v, val.v + val.d.size(), b);
     }
 
-    void TensorTools::RandomizeNormal(real mean, real stddev, Tensor& val) {
-      normal_distribution<real> distribution(mean, stddev);
+    void TensorTools::RandomizeNormal(cnn::real mean, cnn::real stddev, Tensor& val) {
+      normal_distribution<cnn::real> distribution(mean, stddev);
       auto b = [&] {return distribution(*rndeng);};
       generate(val.v, val.v + val.d.size(), b);
     }
