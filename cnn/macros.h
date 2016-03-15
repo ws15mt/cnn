60345897301642260900541ba1933a392574e9b0
U-REDMOND\kaisheny <kaisheny@kaishengw541.redmond.corp.microsoft.com>
2016-03-15 09:58:25 -0700

    define CPUDEVICE and define USE_CPU_FOR_LOOKUP_PARAM so that by default lookup parameters use CPU devices

--- cnn/macros.h
+++ cnn/macros.h
@@ -11,6 +11,8 @@ typedef float real;
 
 /// for memory alignment
 #define ALIGN 6
+#define CNN_ALIGN 64
+// 2^ALIGN = 2^6 = 64
 
 #define ENCODER_LAYER 0
 #define INTENTION_LAYER 1
@@ -42,4 +44,12 @@ typedef float real;
 /// preallocate a GPU memory of consts 1/k
 /// the following is the maximum numbers [1/2,1/3,...1/(MEM_PRE_ALLOCATED_CONSTS_NUMBERS+1)]
 #define MEM_PRE_ALLOCATED_CONSTS_NUMBERS 100
+
+
+/// for device id
+#define CPUDEVICE -1
+
+/// this is defined if having lookup table parameters stored in CPU, so that large model can still be used
+/// other parameters will be stored on CPU or GPU depending on the build
+#define USE_CPU_FOR_LOOKUP_PARAM
 };
